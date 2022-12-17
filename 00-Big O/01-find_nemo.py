c7d7ec1cda5bdce7e6e431ff20137d5aeebc274f
Rayhan <prog.rayhan1@gmail.com>
2022-12-17 11:01:59 +0600

    find nemo in python

---
+++ 00-Big O/01-find_nemo.py
@@ -0,0 +1,11 @@
+# python list
+nemo = ['nemo']
+
+# declare function
+def findNemo(array):
+    for i in array:
+        if i == 'nemo':
+            print(f'found {i}')
+
+# invoke or call function
+findNemo(nemo)
